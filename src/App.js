02dacab81292c104c0b124cb8cf2420694b607a5
Thanh Binh <thanhbinhgtv@gmail.com>
2022-04-18 16:48:40 +0700

    Demo Multi Language in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import { changeLanguage } from 'i18next';
 
 function App() {
+  const { t } = useTranslation()
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <button className='App-btn' onClick={() => changeLanguage('en')}>En</button>
+          <button className='App-btn' onClick={() => changeLanguage('vi')}>Vi</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('content.title')}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://viblo.asia/p/huong-dan-da-ngon-ngu-cho-du-an-reactjs-su-dung-react-i18next-LzD5da8EKjY"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('content.description')}
         </a>
       </header>
     </div>
